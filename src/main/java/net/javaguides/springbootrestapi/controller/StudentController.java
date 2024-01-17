91ef35d4cd7501eacadf789418d254c639c04091
Gyulo94 <qkrcjfrb112@gmail.com>
2024-01-17 20:06:43 +0900

    @PostMapping and @RequestBody

--- src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import net.javaguides.springbootrestapi.bean.Student;
 
@@ -47,5 +51,16 @@ public class StudentController {
     public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName,
             @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }
+
+    // Spring boot REST API that handles HTTP POST Request
+    // @PostMapping and @RequestBody
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 }
